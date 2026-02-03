30d8fefdeda7d6986982a2270b95493a319925cf
Akshay Sawant <akshaysawant917@gmail.com>
2026-02-03 20:31:46 +0530

    updated before deployemnt

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-import dotenv from "dotenv";
-import app from "./app.js";
+// import dotenv from "dotenv";
+// import app from "./app.js";
 
-dotenv.config();
+// dotenv.config();
+
+// const PORT = process.env.PORT || 4000;
+
+// app.listen(PORT, () => {
+//   console.log(
+//     `${process.env.APP_NAME} running on http://localhost:${PORT}`
+//   );
+// });
+
+
+
+import dotenv from "dotenv";
+dotenv.config(); 
+
+import app from "./app.js";
+import prisma from "./prismaClient.js";
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(
-    `${process.env.APP_NAME} running on http://localhost:${PORT}`
-  );
-});
+async function startServer() {
+  try {
+    await prisma.$connect();
+    console.log("✅ Database connected");
+
+    app.listen(PORT, () => {
+      console.log(
+        `${process.env.APP_NAME || "MiniDesk"} running on http://localhost:${PORT}`
+      );
+    });
+  } catch (err) {
+    console.error("❌ Failed to connect to database", err);
+    process.exit(1); // ❗ fail fast
+  }
+}
+
+startServer();
